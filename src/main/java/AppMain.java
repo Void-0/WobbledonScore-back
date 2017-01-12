84f4ae3f7b6d08f62ba56ae57daff1c08def1007
Luka <kosoricluka@gmail.com>
2017-01-12 17:00:32 +0100

    AppMain refactoring for better code readability ScoreWebSocketHandler is now an inner class of AppMain Added missing jdoc

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -1,59 +1,99 @@
 import com.google.gson.Gson;
-import spark.Service;
 import org.eclipse.jetty.websocket.api.*;
+import org.eclipse.jetty.websocket.api.annotations.*;
+import spark.Service;
 import java.util.*;
 import java.util.concurrent.*;
 
-public class AppMain {
+public class AppMain implements Runnable {
     // this map is shared between sessions and threads, so it needs to be thread-safe
-    static Map<Session, String> userUsernameMap = new ConcurrentHashMap<>();
-    static int nextUserNumber = 1;
-    static Match match = null;
-    static DatabaseMock mockDB = null;
+    private final Map<Session, String> userUsernameMap;
+    private int nextUserNumber;
+    private Match match;
+    private final DatabaseMock mockDB;
 
+    /**
+     * Main application default constructor.
+     */
+    public AppMain() {
+        userUsernameMap = new ConcurrentHashMap<>();
+        nextUserNumber = 1;
+        match = null;
+        mockDB = new DatabaseMock();
+    }
+
+    /**
+     * Application main method.
+     *
+     * @param args app args.
+     */
     public static void main(String[] args) {
-        AppMain.mockDB = new DatabaseMock();
+        AppMain appMain = new AppMain();
+        appMain.run();
+    }
 
+    /**
+     * Initialisation and simulation start.
+     */
+    @Override
+    public void run() {
+        initializeHttpService();
+        startMatchSimulation(0, 1, 5, 3);
+    }
+
+    /**
+     * Initialization of match simulation including match parameters and scheduling.
+     *
+     * @param playerA_ID the DB id of the first player.
+     * @param playerB_ID the DB id of the second player.
+     * @param initialDelay the initial simulation delay.
+     * @param period interval at which the simulation progresses.
+     */
+    private void startMatchSimulation(int playerA_ID, int playerB_ID, int initialDelay, int period) {
+        match = new Match(
+                mockDB.getPlayerList().get(playerA_ID),
+                mockDB.getPlayerList().get(playerB_ID)
+        );
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(() -> {
+            if(!match.hasEnded()) {
+                if (ThreadLocalRandom.current().nextBoolean()) {
+                    match.grantPointA();
+                } else {
+                    match.grantPointB();
+                }
+
+                broadcastScore(match);
+            }
+        }, initialDelay, period, TimeUnit.SECONDS);
+    }
+
+    /**
+     * Http service related initialization including CRUD and websocket.
+     */
+    private void initializeHttpService() {
         Gson gson = new Gson();
         Service http = Service.ignite();
 
         http.port(9999);
 
         // declaring websocket to watch the score
-        http.webSocket("/score", ScoreWebSocketHandler.class);
+        http.webSocket("/score", new ScoreWebSocketHandler());
 
         http.before((request, response) -> {
             response.type("application/json");
         });
 
-        http.get("/players", (req, res) -> AppMain.mockDB.getPlayerList(), gson::toJson);
-
-
-        AppMain.match = new Match(
-                AppMain.mockDB.getPlayerList().get(0),
-                AppMain.mockDB.getPlayerList().get(1)
-        );
-
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        scheduler.scheduleAtFixedRate(() -> {
-            if(!AppMain.match.hasEnded()) {
-                if (ThreadLocalRandom.current().nextBoolean()) {
-                    AppMain.match.grantPointA();
-                } else {
-                    AppMain.match.grantPointB();
-                }
-
-                AppMain.broadcastScore(AppMain.match);
-            }
-        }, 5, 5, TimeUnit.SECONDS);
+        http.get("/players", (req, res) -> mockDB.getPlayerList(), gson::toJson);
     }
 
     /**
-     * Sends a message from one user to all users, along with a list of current usernames
+     * Sends current match score to all connected user trough websockets.
      *
-     * @param match
+     * @param match the current match whose score the users are following.
      */
-    public static void broadcastScore(Match match) {
+    public void broadcastScore(Match match) {
         userUsernameMap.keySet().stream().filter(Session::isOpen).forEach(session -> {
             try {
                 session.getRemote().sendString(new Gson().toJson(match));
@@ -61,5 +101,49 @@ public class AppMain {
                 e.printStackTrace();
             }
         });
+    }
+
+    /**
+     * Web socket related operation handling.
+     */
+    @WebSocket
+    public class ScoreWebSocketHandler {
+
+        /**
+         * Saves the new session user on websocket connect and sends current match score.
+         *
+         * @param user the new session user.
+         * @throws Exception connection exception.
+         */
+        @OnWebSocketConnect
+        public void onConnect(Session user) throws Exception {
+            String username = "User" + nextUserNumber++;
+            userUsernameMap.put(user, username);
+            broadcastScore(match);
+        }
+
+        /**
+         * Removes user on websocket close.
+         *
+         * @param user the session user.
+         * @param statusCode the status code.
+         * @param reason the close reson.
+         */
+        @OnWebSocketClose
+        public void onClose(Session user, int statusCode, String reason) {
+            userUsernameMap.remove(user);
+        }
+
+        /**
+         * Resends score through websockets at user demand.
+         *
+         * @param user session user.
+         * @param message received user message.
+         */
+        @OnWebSocketMessage
+        public void onMessage(Session user, String message) {
+            broadcastScore(match);
+        }
+
     }
 }
